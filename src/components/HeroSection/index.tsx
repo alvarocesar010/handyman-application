4245f86b14f15b2b98d28c07b4aebf5f89999915
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-23 11:48:07 +0100

    chore: initial cleaning

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,11 @@
+type HeroSectionProps = {
+  props: string;
+};
+
+const HeroSection = ({ props }: HeroSectionProps) => {
+  return <div className="">
+    <h1></h1>
+  </div>;
+};
+
+export default HeroSection;
