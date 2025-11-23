c07b950f9b022d77066d9d654058dbce19eb4720
Alvaro Souza <alvarocesar010@hotmail.com>
2025-11-23 12:25:16 +0000

    implemented try/cacth in admin/login

--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -2,37 +2,40 @@ import { NextResponse } from "next/server";
 import { signAdminJwt } from "@/lib/auth";
 
 export async function POST(req: Request) {
-  const form = await req.formData();
-  const username = String(form.get("username") ?? "");
-  const password = String(form.get("password") ?? "");
+  try {
+    const form = await req.formData();
+    const username = String(form.get("username") ?? "");
+    const password = String(form.get("password") ?? "");
 
-  // wrong user/pass -> back to /admin/login with ?error=1
-  if (
-    username !== process.env.ADMIN_USER ||
-    password !== process.env.ADMIN_PASS
-  ) {
-    const url = new URL("/admin/login", req.url);
-    url.searchParams.set("error", "1");
-    return NextResponse.redirect(url);
+    if (
+      username !== process.env.ADMIN_USER ||
+      password !== process.env.ADMIN_PASS
+    ) {
+      const url = new URL("/admin/login", req.url);
+      url.searchParams.set("error", "1");
+      return NextResponse.redirect(url);
+    }
+
+    const token = await signAdminJwt({
+      sub: "admin",
+      username,
+      role: "admin",
+    });
+
+    const { origin } = new URL(req.url);
+    const res = NextResponse.redirect(`${origin}/admin/bookings`);
+
+    res.cookies.set("admin_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 8,
+    });
+
+    return res;
+  } catch (err) {
+    console.error("LOGIN API ERROR", err);
+    return new NextResponse("Internal error", { status: 500 });
   }
-
-  const token = await signAdminJwt({
-    sub: "admin",
-    username,
-    role: "admin",
-  });
-
-  // use the same origin as the current request
-  const { origin } = new URL(req.url);
-  const res = NextResponse.redirect(`${origin}/admin/bookings`);
-
-  res.cookies.set("admin_token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    path: "/",
-    maxAge: 60 * 60 * 8, // 8h
-  });
-
-  return res;
 }
