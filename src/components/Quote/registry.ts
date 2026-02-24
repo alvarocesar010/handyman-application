069353d5db982d91009339535210a4c51941c06e
Alvaro Souza <alvarocesar010@hotmail.com>
2026-02-24 14:55:22 +0000

    added logo LL and removed the component Quote

--- src/components/Quote/registry.ts
+++
@@ -1,9 +0,0 @@
-// src/components/Quote/registry.ts
-import { FURNITURE_REGISTRY } from "./furniture/registry";
-import { CURTAIN_REGISTRY } from "./curtain/registry";
-import type { QuoteRegistryItem } from "./registry.types";
-
-export const QUOTE_ITEM_REGISTRY: QuoteRegistryItem[] = [
-  ...FURNITURE_REGISTRY,
-  ...CURTAIN_REGISTRY,
-];
